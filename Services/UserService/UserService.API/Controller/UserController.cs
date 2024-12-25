4b5a7c608865abd59c3a3c0748e547e21833dad7
HuyHoangFSDev <nguyenthehuyhoang2002@gmail.com>
2024-12-25 22:25:24 +0700

    .gitignore

---
+++ Services/UserService/UserService.API/Controller/UserController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.API.Controller;
+
+[Route("api/user")]
+[ApiController]
+public class UserController : Microsoft.AspNetCore.Mvc.Controller
+{
+    
+}
--- Services/UserService/UserService.Domain/obj/Debug/net8.0/UserService.Domain.AssemblyInfo.cs
+++ Services/UserService/UserService.Domain/obj/Debug/net8.0/UserService.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8b954da5d25974b9ef348f9a1cc6abb4b0d74f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
