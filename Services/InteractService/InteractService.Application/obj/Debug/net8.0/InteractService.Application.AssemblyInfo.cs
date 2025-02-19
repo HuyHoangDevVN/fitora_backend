c97d50e3816695c79d75b2dd6f58d3a2ad794744
HuyHoangFSDev <nguyenthehuyhoang2002@gmail.com>
2025-02-19 09:54:20 +0700

    UPDATE

--- Services/InteractService/InteractService.Application/obj/Debug/net8.0/InteractService.Application.AssemblyInfo.cs
+++ Services/InteractService/InteractService.Application/obj/Debug/net8.0/InteractService.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InteractService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f121221c9d7afe59dfabdc272cbb7f3bc893ea4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf25eaeea60621c1f09aa7f2aaf3944ee32555e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("InteractService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InteractService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
