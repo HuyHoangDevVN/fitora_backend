d91b4d3b87a6adc63a7806daa51a1b4265a772e4
HuyHoangFSDev <nguyenthehuyhoang2002@gmail.com>
2025-05-28 17:58:13 +0700

    Add workflow

--- Services/AuthService/AuthService.Infrastructure/obj/Debug/net8.0/AuthService.Infrastructure.AssemblyInfo.cs
+++ Services/AuthService/AuthService.Infrastructure/obj/Debug/net8.0/AuthService.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+122c1b54e8f6899aaf31247d13a450886f0f238f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6354c2e40c0e63ac746a1fc9749d3d171470d14")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
